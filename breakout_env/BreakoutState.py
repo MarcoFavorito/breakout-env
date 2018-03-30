3e599c2021cced87a40eee4bdb85f0510cd00d1b
Marco Favorito <marco.favorito@gmail.com>
2018-03-31 00:06:43 +0200

    splitted implementation of Breakout env.

---
+++ breakout_env/BreakoutState.py
@@ -0,0 +1,272 @@
+import random
+
+from breakout_env.utils import GameObject, Bricks, FRAME_X, aabb, FRAME_Y, actions_meaning, obs_base, render_bb, digits, \
+    BRICKS_COLS, BRICKS_SIZE
+import numpy as np
+import copy
+
+class BreakoutState(object):
+
+    def __init__(self, conf):
+        self.conf = conf
+        self.reset()
+
+    def reset(self):
+        self.score = 0
+        self.reward = 0
+        self.step_count = 0
+        self.lifes = self.conf["lifes"]
+        self.max_step = self.conf["max_step"]
+        self.terminal = False
+        self.started = False
+        # self.ball = GameObject(self.conf['ball_pos'], self.conf['ball_size'], self.conf['ball_color'])
+        # self.ball_v = list(self.conf['ball_speed'])
+        self.ball, self.ball_v = self._get_random_ball()
+
+        self.paddle = GameObject([189, 70], [4, self.conf['paddle_width']], self.conf['paddle_color'], self.conf['catch_reward'])
+        self.paddle_v = [0, self.conf['paddle_speed']]
+        self.bricks = Bricks(self.conf['bricks_rows'], BRICKS_COLS, BRICKS_SIZE, self.conf['bricks_color'],
+                             self.conf['bricks_reward'])
+
+    def _get_random_ball(self):
+
+        # if random.random()<0.5:
+        #
+        #     ball = GameObject([100, 10], self.conf['ball_size'], self.conf['ball_color'])
+        #     random_v = list(self.conf['ball_speed'])
+        #     random_v[1] = abs(random_v[1])
+        #
+        # else:
+        #
+        #     ball = GameObject([100, 120], self.conf['ball_size'], self.conf['ball_color'])
+        #     random_v = list(self.conf['ball_speed'])
+        #     random_v[1] = -abs(random_v[1])
+
+        ball = GameObject([100, 10], self.conf['ball_size'], self.conf['ball_color'])
+        random_v = list(self.conf['ball_speed'])
+        random_v[1] = abs(random_v[1])
+
+        ball_v = random_v
+
+
+        # ball = GameObject([100, np.random.randint(10, 120)], self.conf['ball_size'], self.conf['ball_color'])
+        # random_v = list(self.conf['ball_speed'])
+        # if random.random()<0.5:
+        #     random_v = random_v[::-1]
+        # random_v[1] *= np.random.choice([-1, 1])
+        # ball_v = random_v
+
+        return ball, ball_v
+
+    def _edge_collision(self):
+        bb1 = self.ball.boundingbox
+        if aabb(bb1, [0, 999, 0, FRAME_X[0]]):  # Left edge
+            self.ball_v = [self.ball_v[0], -self.ball_v[1]]
+            self.ball.translate([0, 2 * self.ball_v[1]])
+        elif aabb(bb1, [0, 999, FRAME_X[1], 999]):  # Right edge
+            self.ball_v = [self.ball_v[0], -self.ball_v[1]]
+            self.ball.translate([0, 2 * self.ball_v[1]])
+        elif aabb(bb1, [0, FRAME_Y[0], 0, 999]):  # Top edge
+            self.ball_v = [-self.ball_v[0], self.ball_v[1]]
+            self.ball.translate([2 * self.ball_v[0], 0])
+        elif aabb(bb1, [FRAME_Y[1], 999, 0, 999]):  # Bottom edge
+            self.lifes -= 1
+            self.terminal = self.started and self.lifes == 0
+            # self.ball = GameObject(self.conf['ball_pos'], self.conf['ball_size'], self.conf['ball_color'])
+            # self.ball_v = self.conf['ball_speed']
+            self.ball, self.ball_v = self._get_random_ball()
+
+
+    def _paddle_collision(self):
+        bb1 = self.ball.boundingbox
+        if aabb(bb1, self.paddle.boundingbox):
+            vy, vx = self.ball_v
+            # print(self.ball_v)
+            bigger, smaller = (vx, vy) if abs(vx) > abs(vy) else (vy, vx)
+            bigger, smaller = abs(bigger), abs(smaller)
+
+            pos_x = (self.ball.pos[1] - (self.paddle.pos[1] + self.paddle.size[1] / 2)) / (self.paddle.size[1] / 2)
+            # print(pos_x, self.ball.pos[1], self.paddle.pos[1] + self.paddle.size[1]/2, self.ball.pos[1] - self.paddle.pos[1] + self.paddle.size[1]/2)
+            vx_sign = 1 if vx > 0 else -1
+            # pos_x = -vx_sign if pos_x < -vx_sign*0.3 else vx_sign
+            pos_x *= vx_sign
+            vx, vy = (-bigger, smaller) if pos_x >= -1.0 and pos_x < -0.75 else \
+                     (-smaller, bigger) if pos_x >= -0.75 and pos_x < -0.25 else \
+                     (vx_sign * vx, vy) if pos_x >= -0.25 and pos_x < 0.25 else \
+                     (smaller, bigger) if pos_x >= 0.25 and pos_x < 0.75 else \
+                     (bigger, smaller)
+
+            # print(vx, vy)
+            vx = vx_sign * vx
+
+            # self.ball_v = [-self.ball_v[0], self.ball_v[1]]
+            self.ball_v = [-vy, vx]
+
+            self.ball.translate([2 * self.ball_v[0], self.ball_v[1]])
+            # Re-new bricks if all clear
+            if len(self.bricks.bricks) == 0:
+                self.bricks = Bricks(self.conf['bricks_rows'], BRICKS_COLS, BRICKS_SIZE, self.conf['bricks_color'],
+                                     self.conf['bricks_reward'])
+
+            return self.paddle.reward
+        return 0
+
+    def _bricks_collision(self):
+        bb1 = self.ball.boundingbox
+        outer_bb = self.bricks.outer_boundingbox
+
+        # Early return if not inside outer bounding box
+        if not aabb(bb1, outer_bb):
+            return 0
+
+        x2 = (bb1[2] + bb1[3]) / 2.0
+        # y2 = (bb1[0] + bb1[1]) / 2.0
+        x1 = x2 - self.ball_v[1]
+        # y1 = y2 - self.ball_v[0]
+
+        for idx, brick in enumerate(self.bricks.bricks):
+            bb2 = brick.boundingbox
+
+            if not aabb(bb1, bb2):
+                continue
+
+            if (x1 < bb2[2] and x2 > bb2[2]) or (x1 > bb2[3] and x2 < bb2[3]):
+                self.ball_v = [self.ball_v[0], -self.ball_v[1]]
+                self.ball.translate([0, 2 * self.ball_v[1]])
+                # continue
+            else:
+                self.ball_v = [-self.ball_v[0], self.ball_v[1]]
+                self.ball.translate([2 * self.ball_v[0], 0])
+
+            r = brick.reward
+            del self.bricks.bricks[idx]
+            return r
+
+        return 0
+
+    def encode(self):
+        obs_type = self.conf["observation"]
+        res = None
+        if obs_type == "number":
+            res = self.encode_number()
+        elif obs_type == "vector":
+            res = self.encode_vector()
+        elif obs_type == "pixels":
+            res = self.encode_pixels()
+        elif obs_type == "number_discretized":
+            res = self.encode_number_discretized()
+        else:
+            raise Exception
+        return res
+
+
+    def encode_number(self):
+        """encode the state as a number"""
+        i = self.paddle.pos[1]
+        i *= 160
+        i += self.ball.pos[0]
+        i *= 210
+        i += self.ball.pos[1]
+        i *= 160
+        i += self.ball_v[0]
+        i *= 4
+        i += self.ball_v[1]
+        i *= 4
+        return i
+
+    def encode_number_discretized(self):
+        """encode the state as a number with an efficient state discretization"""
+        paddle_x = self.paddle.pos[1]//2 #4
+        ball_x = self.ball.pos[1]//2
+        ball_y = self.ball.pos[0]//2
+        # ball_y = 0 if ball_y in range(0, 93) else (ball_y-93)//2 if ball_y in range(93, FRAME_Y[1]) else None
+        # if ball_y is None:
+        #     raise Exception
+
+        ball_v = self.ball_v
+        velocities = [-2, -1, 1, 2]
+        ball_vy = velocities.index(ball_v[0])
+        ball_vx = velocities.index(ball_v[1])
+
+
+        i = paddle_x
+        i *= 160//2
+        i += ball_x
+        i *= 160//2
+        # i += ball_vy
+        # i *= 4
+        # i += ball_vx
+        # i *= 4
+
+        i += ball_y
+
+        # i = paddle_x - ball_x + 160
+        # print(paddle_x, ball_x, paddle_x-ball_x, i)
+        # i*=320
+        # i+= ball_y
+
+        return i
+
+
+    def encode_vector(self):
+        # paddle_x, ball_y, ball_x, ball_speed_y, ball_speed_x
+        return np.asarray([self.paddle.pos[1], self.ball.pos[0], self.ball.pos[1], self.ball_v[0], self.ball_v[1]])
+
+    def encode_pixels(self):
+        obs = np.copy(obs_base)
+
+        # Draw paddle
+        paddle_bb = self.paddle.boundingbox
+        obs[paddle_bb[0]:paddle_bb[1], paddle_bb[2]:paddle_bb[3]] = self.paddle.color
+
+        # Draw bricks
+        for brick in self.bricks.bricks:
+            bb = brick.boundingbox
+            obs[bb[0]:bb[1], bb[2]:bb[3]] = brick.color
+
+        # Draw ball
+        if self.started:
+            ball_bb = self.ball.boundingbox
+            obs[ball_bb[0]:ball_bb[1], ball_bb[2]:ball_bb[3]] = self.ball.color
+
+        # Draw info (score, lifes)
+        life_bb = render_bb['lifes']
+        obs[life_bb[0]:life_bb[1], life_bb[2]:life_bb[3]] = digits[self.lifes]
+
+        scores_bb = render_bb['scores']
+        scores = [(self.score // 10 ** i) % 10 for i in range(2, -1, -1)]
+        for idx, bb in enumerate(scores_bb):
+            obs[bb[0]:bb[1], bb[2]:bb[3]] = digits[scores[idx]]
+
+        return obs
+
+    def _next_state(self, action):
+        previous_lives = self.lifes
+        act = actions_meaning[action]
+
+        if act == 'RIGHT' and self.paddle.boundingbox[3] + self.paddle_v[1] < FRAME_X[1] + 8:
+            self.paddle.translate(self.paddle_v)
+        if act == 'LEFT' and self.paddle.boundingbox[2] - self.paddle_v[1] > FRAME_X[0] - 8:
+            self.paddle.translate([-x for x in self.paddle_v])
+
+        if self.started:
+            self.ball.translate(self.ball_v)
+
+            # Check collision
+            self._edge_collision()
+            self.reward = self._paddle_collision()
+            self.reward += self._bricks_collision()
+            self.score += self.reward
+
+        # Check is FIRE
+        if actions_meaning[action] == 'FIRE':
+            self.started = True
+
+        self.step_count += 1
+        if self.step_count >= self.max_step:
+            self.terminal = True
+
+        if self.lifes < previous_lives:
+            self.reward = -10
+        return self
+
---
+++ breakout_env/utils.py
@@ -0,0 +1,105 @@
+import copy
+import numpy as np
+import os
+
+FRAME_X = [7, 152]
+FRAME_Y = [31, 194]
+
+default_conf = {
+  'max_step': 10000,
+  'lifes': 5,
+  'ball_pos': [100, 40],
+  'ball_speed': [2, 1],
+  'ball_color': 143,
+  'ball_size': [5, 2],
+  'paddle_width': 15,
+  'paddle_color': 143,
+  'paddle_speed': 2,
+  'bricks_rows': 6,
+  'bricks_color': [200, 180, 160, 140, 120, 100],
+  'bricks_reward': [6, 5, 4, 3, 2, 1],
+  'catch_reward': 0,
+  #   possible values: {"pixels", "number", "number_discretized", "vector"}
+  'observation': "pixels",
+}
+
+actions_meaning = ['NOOP', 'FIRE', 'RIGHT', 'LEFT']
+
+obs_base = np.load(os.path.join(os.path.dirname(__file__), 'assets', 'base.npy'))
+digits = [np.load(os.path.join(os.path.dirname(__file__), 'assets', '{}.npy'.format(i))) for i in range(10)]
+render_bb = {
+      'scores': [[5, 15, 36, 48], [5, 15, 52, 64], [5, 15, 68, 80]],
+      'lifes': [5, 15, 100, 112],
+      'level': [5, 15, 128, 140]
+    }
+
+BRICKS_COLS = 18
+BRICKS_SIZE = [6, 8]
+
+
+class GameObject(object):
+    def __init__(self, pos, size, color=143, reward=0):
+        self.pos = list(pos)
+        self.size = list(size)
+        self.color = color
+        self.reward = reward
+
+    def translate(self, translate):
+        # res = copy.deepcopy(self)
+        self.pos[0] += translate[0]
+        self.pos[1] += translate[1]
+        # return res
+
+    @property
+    def boundingbox(self):
+        # BB = (y1, y2, x1, x2)
+        # 0 ------ 1
+        # |        |
+        # |        |
+        # 2 ------ 3
+        return [self.pos[0], self.pos[0] + self.size[0], self.pos[1], self.pos[1] + self.size[1]]
+
+    @property
+    def center(self):
+        x = self.pos[1] + self.size[1] / 2.0
+        y = self.pos[0] + self.size[0] / 2.0
+        return (y, x)
+
+
+# A warpper of all bricks GameObject
+class Bricks(object):
+    def __init__(self, rows, cols, brick_size, brick_colors, brick_rewards):
+        assert (len(brick_colors) == len(brick_rewards) == rows)
+        self.bricks_pos = [57, 8]
+        self.rows = rows
+        self.cols = cols
+        self.brick_size = list(brick_size)
+        self.brick_colors = list(brick_colors)
+        self.brick_rewards = list(brick_rewards)
+        self.bricks = []
+
+        for r in range(self.rows):
+            y = self.bricks_pos[0] + r * self.brick_size[0]
+            x = self.bricks_pos[1]
+            row_bricks = self.__create_rows([y, x], 200 - 20 * r, self.rows - r)
+            self.bricks += row_bricks
+
+    def __create_rows(self, pos, c, r):
+        rows = [GameObject([pos[0], pos[1] + p * self.brick_size[1]], self.brick_size, c, r) for p in range(self.cols)]
+        return rows
+
+    @property
+    def outer_boundingbox(self):
+        return [
+            self.bricks_pos[0],
+            self.bricks_pos[0] + self.brick_size[0] * self.rows,
+            self.bricks_pos[1],
+            self.bricks_pos[1] + self.brick_size[1] * self.cols
+        ]
+
+# Collision detection
+def aabb(bb1, bb2):
+  if bb1[0] < bb2[0]:
+    return bb1[0] < bb2[1] and bb1[1] > bb2[0] and bb1[2] < bb2[3] and bb1[3] > bb2[2]
+  else:
+    return bb2[0] < bb1[1] and bb2[1] > bb1[0] and bb2[2] < bb1[3] and bb2[3] > bb1[2]
